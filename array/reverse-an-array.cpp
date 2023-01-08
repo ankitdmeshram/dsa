521f72cfadf425b6a5cc2a2f797b6473017e4900
Ankit Meshram <ankitdm69@gmail.com>
2023-01-08 11:15:27 +0530

    Reverse of an array using third variable

---
+++ array/reverse-an-array.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int n;
+    cout << "Enter size of an array" << endl;
+    cin >> n;
+    int arr[n];
+   
+    cout << "Enter the element of an array " << endl;
+    for(int i=0; i<n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    cout << "Before Reversing" << endl;
+    for(int i=0; i<n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl << "After Reversing" << endl;
+    for(int i=0; i<n/2; i++)
+    {
+        // using 3rd temprory variable
+        int temp; 
+        temp = arr[i];     
+        arr[i]=arr[n-i-1];    
+        arr[n-i-1]=temp;          
+    }
+    for(int i=0; i<n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
