e0640e24d20f7ce3940dd0e4a6001ab75b2d4e1b
Ankit Meshram <ankitdm69@gmail.com>
2023-01-20 07:00:06 +0530

    Sorting with standard library

--- array/sorting-of-an-array.cpp
+++ array/sorting-of-an-array.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
@@ -14,41 +15,44 @@ int main()
 
     cout << "Ascending order" << endl;
    
-    for(int i=0; i<n; i++)
-    {
-        for(int j=0; j<n; j++)
-        {
-            if(arr[i]<arr[j])
-            { 
-                arr[i]=arr[i]+arr[j];   
-                arr[j]=arr[i]-arr[j]; 
-                arr[i]=arr[i]-arr[j];  
-            }
-        }
-    }
+    sort(arr, arr+n);
+
+//     for(int i=0; i<n; i++)
+//     {
+//         for(int j=0; j<n; j++)
+//         {
+//             if(arr[i]<arr[j])
+//             { 
+//                 arr[i]=arr[i]+arr[j];   
+//                 arr[j]=arr[i]-arr[j]; 
+//                 arr[i]=arr[i]-arr[j];  
+//             }
+//         }
+//     }
+
     for(int i=0; i<n; i++)
     {
         cout << arr[i] << " ";
     }
 
     cout << endl << "Descending order" << endl;
+    sort(arr, arr+n, greater<int>());
 
-for(int i=0; i<n; i++)
-    {
-        for(int j=0; j<n; j++)
-        {
-            if(arr[i]>arr[j])
-            { 
-                arr[i]=arr[i]+arr[j];   
-                arr[j]=arr[i]-arr[j]; 
-                arr[i]=arr[i]-arr[j];  
-            }
-        }
-    }
+// for(int i=0; i<n; i++)
+//     {
+//         for(int j=0; j<n; j++)
+//         {
+//             if(arr[i]>arr[j])
+//             { 
+//                 arr[i]=arr[i]+arr[j];   
+//                 arr[j]=arr[i]-arr[j]; 
+//                 arr[i]=arr[i]-arr[j];  
+//             }
+//         }
+//     }
+
     for(int i=0; i<n; i++)
     {
         cout << arr[i] << " ";
     }
-
-    
 }
--- array/tempCodeRunnerFile.cpp
+++ array/tempCodeRunnerFile.cpp
@@ -1,1 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int n;
+    cout << "Enter size of an array" << endl;
+    cin >> n;
+    int arr[n];
+   
+    cout << "Enter the element of an array " << endl;
     for(int i=0; i<n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    cout << "Before Reversing" << endl;
+    for(int i=0; i<n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl << "After Reversing" << endl;
+    for(int i=0; i<n/2; i++)
+    {
+        // using 3rd temprory variable
+        // int temp; 
+        // temp = arr[i];     
+        // arr[i]=arr[n-i-1];    
+        // arr[n-i-1]=temp;      
+
+        // without using third variable
+        arr[i]=arr[i]+arr[n-i-1];  
+        arr[n-i-1]=arr[i]-arr[n-i-1];
+        arr[i]=arr[i]-arr[n-i-1]; 
+
+    }
+    for(int i=0; i<n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
